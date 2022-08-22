3c27bb9ece2521189dd0bf2ccf4dcdf900405fff
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2022-08-22 16:39:52 +0200

    Creating Application Class

---
+++ lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  // const MyApp({Key? key}) : super(key: key); //default constructor
+
+  // named constructor
+  // ignore: prefer_const_constructors_in_immutables
+   MyApp._internal();
+
+  static final MyApp _instance = MyApp._internal();  //Singletone or Single instance
+
+  factory MyApp() => _instance; //factory
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
